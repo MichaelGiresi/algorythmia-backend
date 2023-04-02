b14d8e79ea96f66ac3faca289d27edab53e4b8e7
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2023-04-01 19:02:02 -0700

    Add files via upload
    New spring boot app, created mysql database. React, spring boot and mysql communicate. Tested api fetch

---
+++ spring-boot-algorythmia/src/main/java/com/algorythmia/springbootalgorythmia/SpringBootAlgorythmiaApplication.java
@@ -0,0 +1,13 @@
+package com.algorythmia.springbootalgorythmia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAlgorythmiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAlgorythmiaApplication.class, args);
+	}
+
+}
