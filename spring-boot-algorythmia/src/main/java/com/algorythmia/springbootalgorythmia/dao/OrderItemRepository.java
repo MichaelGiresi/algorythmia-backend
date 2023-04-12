cd063b3125dd7d24db62d39c6a7166c59eab8f5d
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2023-04-11 22:01:54 -0400

    Delete spring-boot-algorythmia directory

--- spring-boot-algorythmia/src/main/java/com/algorythmia/springbootalgorythmia/dao/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.algorythmia.springbootalgorythmia.dao;
-
-import com.algorythmia.springbootalgorythmia.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
