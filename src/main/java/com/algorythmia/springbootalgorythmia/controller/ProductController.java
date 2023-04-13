acb43b97035350a380f0a83c86637224b6b4ce38
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2023-04-12 20:41:50 -0400

    Add files via upload
    Added Delete Mapping in Product Controller

--- src/main/java/com/algorythmia/springbootalgorythmia/controller/ProductController.java
+++ src/main/java/com/algorythmia/springbootalgorythmia/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.algorythmia.springbootalgorythmia.controller;
 
 import com.algorythmia.springbootalgorythmia.dao.ProductRepository;
+import com.algorythmia.springbootalgorythmia.entity.Order;
 import com.algorythmia.springbootalgorythmia.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,15 +9,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("api/products")
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+
 
     @PostMapping("/")
     public ResponseEntity<?> createProduct(@RequestBody Product product) {
@@ -25,6 +29,16 @@ public class ProductController {
         response.put("message", "Product created");
 
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return ResponseEntity.ok("Product with ID " + id + " has been deleted.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with ID " + id + " not found.");
+        }
     }
 
 }
